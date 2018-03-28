2d4539674d32ce21200bce2e0dfd4f0e32aa9fcd
Kris Hale <karissa.hale72@gmail.com>
2018-03-28 17:50:44 -0400

    added a modal click event on a submit button for a form element in .parallax5 div

---
+++ modal.js
@@ -0,0 +1,15 @@
+//This Javascript Program is being used to send form Data from a bootstrap created from, to a modal created in bootsrap, that displays a completed message to the user. This Program helps to demonstrate the functionality of a would-be back end or server side functionality to the form that would store the input data, and return a message of completion
+
+
+//in .parallax5, we will be adding a click event listener to a form submit button to then toggle the bootsrap modal to appear and display a completed for submission message
+
+
+
+$('#myModal').modal(show);
+
+
+//the modal will have a click event that causes it to close
+
+//upon closing the modal, the form will dissapear, and the grid view project feed will expand to fill the space
+
+
