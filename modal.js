4823d5ed0867a9ef70a10b1e09734bad56fbaea7
Kris Hale <karissa.hale72@gmail.com>
2018-03-29 17:00:07 -0400

    removed redundnacies in css and html to resolve console errors

--- modal.js
+++ modal.js
@@ -3,13 +3,54 @@
 
 //in .parallax5, we will be adding a click event listener to a form submit button to then toggle the bootsrap modal to appear and display a completed for submission message
 
+    $('#myModal').modal(show);
 
 
-$('#myModal').modal(show);
 
 
 //the modal will have a click event that causes it to close
 
+
+
 //upon closing the modal, the form will dissapear, and the grid view project feed will expand to fill the space
 
+// $(document).on('hidden.bs.modal', '#myModal', function () {
+//     $("#formToggle").hide();
+//    });
+
+   $(document).ready( function() {
+    
+    $('#formToggle').on('close', function(){
+        $('#formToggle').attr('style', display = "none");
+    });
+});
+
+
+
+
+
+
+
+
+
+// var form = document.querySelector('form');
+// form.style.display = "none";
+
+//$("#specific-form-id").remove();
+
+
+
+
+//a possible circular animation to use on footer links
+
+// $("#anything").circulate({
+
+//     speed: 400,                  // Speed of each quarter segment of animation, 1000 = 1 second
+//     height: 200,                 // Distance vertically to travel
+//     width: 200,                  // Distance horizontally to travel
+//     sizeAdjustment: 100,         // Percentage to grow or shrink
+//     loop: false,                 // Circulate continuously
+//     zIndexValues: [1, 1, 1, 1],  // Sets z-index value at each stop of animation
+
+// });
 
